7b3b7a4590cead1766409fc97456018cb4bf24f8
christviebidzimou310-jpg <christviebidzimou310@gmail.com>
2026-07-01 11:39:44 +0200

    Create data_fetcher.py

---
+++ data_fetcher.py
@@ -0,0 +1,13 @@
+def recuperer_matchs_en_ligne():
+    return [
+        {
+            "match": "Real Madrid vs Valence",
+            "force_dom": 89, "forme_dom": 87, "force_ext": 71, "forme_ext": 64,
+            "finisher_absent": False, "conflit_interne": False, "meteo_extreme": False, "cote_bookmaker": 1.45
+        },
+        {
+            "match": "Arsenal vs Chelsea",
+            "force_dom": 81, "forme_dom": 74, "force_ext": 83, "forme_ext": 72,
+            "finisher_absent": True, "conflit_interne": True, "meteo_extreme": True, "cote_bookmaker": 1.95
+        }
+    ]
